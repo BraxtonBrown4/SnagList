44f9bdf1bb9c446dd485357f1d70c0c7124c183f
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-06-13 11:56:06 -0500

    replacing bootstrap with tailwind

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import tailwindcss from '@tailwindcss/vite'
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => {
@@ -16,6 +17,6 @@ export default defineConfig(() => {
     build: {
       outDir: "build",
     },
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
   };
 });
