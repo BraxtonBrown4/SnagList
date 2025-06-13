6d20f4e7fbb5abfb26bc69b3fd56e046bcad57ad
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-06-13 11:03:43 -0500

    auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,34 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { tryGetLoggedInUser } from "./managers/authManager";
+import { Spinner } from "reactstrap";
+import NavBar from "./components/NavBar";
+import ApplicationViews from "./components/ApplicationViews";
 
 export const App = () => {
+  const [loggedInUser, setLoggedInUser] = useState();
+
+  useEffect(() => {
+    // user will be null if not authenticated
+    tryGetLoggedInUser().then((user) => {
+      setLoggedInUser(user);
+    });
+  }, []);
+
+  // wait to get a definite logged-in state before rendering
+  if (loggedInUser === undefined) {
+    return <Spinner />;
+  }
 
   return (
-    <h2>APP</h2>
-  )
+    <>
+      <NavBar loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser} />
+      <ApplicationViews
+        loggedInUser={loggedInUser}
+        setLoggedInUser={setLoggedInUser}
+      />
+    </>
+  );
 }
 
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { NavLink as RRNavLink } from "react-router-dom";
+import {
+  Button,
+  Nav,
+  NavLink,
+  NavItem,
+  Navbar,
+  NavbarBrand,
+} from "reactstrap";
+import { logout } from "../managers/authManager";
+
+export default function NavBar({ loggedInUser, setLoggedInUser }) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div>
+      <Navbar color="light" light fixed="true" expand="lg">
+        <NavbarBrand className="mr-auto" tag={RRNavLink} to="/">
+          SnagList
+        </NavbarBrand>
+        {loggedInUser ? (
+          <>
+            <Button
+              color="primary"
+              onClick={(e) => {
+                e.preventDefault();
+                setOpen(false);
+                logout().then(() => {
+                  setLoggedInUser(null);
+                  setOpen(false);
+                });
+              }}
+            >
+              Logout
+            </Button>
+          </>
+        ) : (
+          <Nav navbar>
+            <NavItem>
+              <NavLink tag={RRNavLink} to="/login">
+                <Button color="primary">Login</Button>
+              </NavLink>
+            </NavItem>
+          </Nav>
+        )}
+      </Navbar>
+    </div>
+  );
+}
