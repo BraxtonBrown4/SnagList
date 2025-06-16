9fbcdeb2373dcc8077329fd2cdfd876ff00f75a2
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-06-16 14:35:50 -0500

    added create item

--- client/src/managers/itemManager.js
+++ client/src/managers/itemManager.js
@@ -5,3 +5,13 @@ export const deleteItemById = (itemId) => {
         method: "DELETE"
     })
 }
+
+export const createItem = (item) => {
+  return fetch(_apiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(item)
+  }).then(res => res.json())
+}
