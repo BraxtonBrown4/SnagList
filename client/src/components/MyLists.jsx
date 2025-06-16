8b92e3ee91bdc1f66e9a990dab7cdab23723f05c
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-06-16 10:50:35 -0500

    added public h3 and css

--- client/src/components/MyLists.jsx
+++ client/src/components/MyLists.jsx
@@ -26,15 +26,21 @@ export const MyLists = ({ loggedInUser }) => {
                         key={l.id}
                         className="max-w-sm mx-auto my-6 bg-white rounded-2xl shadow-md hover:shadow-lg transition-shadow duration-300 border border-gray-200">
                         <div onClick={() => {navigate(`/Lists/${l.id}`)}}>
+                            
                             <div className="p-6">
                                 <h2 className="text-xl font-medium text-gray-900 mb-2">{l.name}</h2>
+
+                                <h3>Public: {l.isPublic ? "True" : "False"}</h3>
+                            </div>
+
+                            <div>
 
                                 {l.tags.slice(0, 2).map((t) => <p key={t.id} className="text-gray-600 text-sm">#{t.name}</p>)}
 
                                 {l.tags.length > 3 && <p className="text-gray-600 text-sm">...</p>}
                             </div>
 
-                            <div>
+                            <div  className="p-6">
                                 {l.items.slice(0, 4).map(i => <p key={i.id} className="text-gray-600 text-sm font-medium">{i.name}</p>)}
                                 {l.items.length > 5 && <p className="text-gray-600 text-sm font-medium">...</p>}
                             </div>
