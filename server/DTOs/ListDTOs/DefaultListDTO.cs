a31b11c2e6bf510519cd4d80c6eabe86b956e875
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-06-19 15:45:03 -0500

    removed excess info from DefaultListDTO added detailed dto

--- server/DTOs/ListDTOs/DefaultListDTO.cs
+++ server/DTOs/ListDTOs/DefaultListDTO.cs
@@ -8,9 +8,6 @@ public class DefaultListDTO
 {
   public int Id { get; set; }
   public int UserProfileId { get; set; }
-  public DefaultUserProfileDTO? UserProfile { get; set; }
   public string Name { get; set; }
   public bool IsPublic { get; set; }
-  public List<DefaultTagDTO> Tags { get; set; }
-  public List<DefaultItemDTO> Items { get; set; }
 }
