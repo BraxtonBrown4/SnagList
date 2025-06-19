ba4487f72f278d17f9b9a52314c1a83c821b4146
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-06-19 09:30:37 -0500

    added api endpoint

---
+++ server/DTOs/UserProfileDTOs/MyUserProfileDTO.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace SnagList.DTOs;
+
+public class MyUserProfileDTO
+{
+    public int Id { get; set; }
+    public string? ProfilePic { get; set; }
+    public string UserName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+    public string Email { get; set; }
+}
