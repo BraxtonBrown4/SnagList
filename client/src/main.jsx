dc080ebed2e2b2343109c8744ba91ba4c1c4c18d
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-06-20 13:34:15 -0500

    set up provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,14 @@ import { App } from "./App";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
-
+const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </BrowserRouter>,
 );
