1ce01238f9028fd1cec918149d15644f4efe5372
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-06-23 13:50:10 -0500

    implemented changes on listdetails

--- client/src/components/ListDetails.jsx
+++ client/src/components/ListDetails.jsx
@@ -1,77 +1,58 @@
 import { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
-import { getMyListById, getPublicListById } from "../managers/listManager"
 import { DeleteModal } from "../modals/DeleteModal"
-import { createItem, deleteItemById } from "../managers/itemManager"
 import { AddItemModal } from "../modals/AddItemModal"
 import { EditItemModal } from "../modals/EditItemModal"
+import { useGetListById } from "../queryHooks/listQueryHooks"
+import { LoadingModal } from "../modals/LoadingModal"
+import { ErrorModal } from "../modals/ErrorModal"
+import { useCreateItem, useDeleteItemById } from "../queryHooks/itemQueryHooks"
 
 export const ListDetails = ({ loggedInUser }) => {
-    const { listId, isPublic } = useParams()
+    const { listId } = useParams()
     const [deleteId, setDeleteId] = useState(0)
-    const [list, setList] = useState({})
     const navigate = useNavigate()
-    const [usersList, setUsersList] = useState(false)
     const [addItemModalOpen, setAddItemModalOpen] = useState(false)
     const [editItem, setEditItem] = useState(null)
     const [newItemArr, setNewItemArr] = useState(null)
 
-    const isUsersList = (list) => {
-        setUsersList(list.userProfileId == loggedInUser.id)
+    const { data, error, isError, isLoading } = useGetListById(parseInt(listId))
+    const { mutateAsync: createItem } = useCreateItem(listId)
+    const { mutateAsync: deleteById } = useDeleteItemById(listId)
+
+    const handleNewItem = async () => {
+        await createItem({ ...newItemArr[0], ListId: listId })
+        setNewItemArr(null)
     }
 
     useEffect(() => {
-        if (listId > 0 && isPublic !== "" && loggedInUser.id > 0 && deleteId == 0 && newItemArr == null && editItem == null) {
-
-            const parsedListId = parseInt(listId)
-            const boolIsPublic = isPublic == "true" ? true : false
-
-            if (boolIsPublic) {
-                getPublicListById(parsedListId).then((res) => {
-                    setList(res)
-                    isUsersList(res)
-                })
-            } else {
-                getMyListById(parsedListId).then((res) => {
-                    if (!res.id) {
-                        navigate("/Unauthorized")
-                    } else {
-                        setList(res)
-                        isUsersList(res)
-                    }
-                })
-            }
-        }
-    }, [loggedInUser, listId, isPublic, deleteId, editItem, newItemArr])
-
-    useEffect(() => {
         if (newItemArr) {
-            createItem({ ...newItemArr[0], ListId: listId }).then(() => setNewItemArr(null))
+            handleNewItem()
         }
     }, [newItemArr])
 
-    return (list.id &&
+    return (
         <div className="fixed inset-0 flex flex-col items-center justify-center bg-gray-50 p-6 overflow-auto">
 
             <div className="w-full max-w-lg bg-white rounded-2xl shadow-lg border border-gray-200">
 
                 <div className="p-8 text-center">
 
-                    <h2 className="text-3xl font-medium text-gray-900 mb-4">{list.name}</h2>
+                    <h2 className="text-3xl font-medium text-gray-900 mb-4">{data?.name}</h2>
 
                     <p
-                        onClick={() => navigate(`/Profile/${list.userProfileId}`)}
+                        onClick={() => navigate(`/Profile/${data?.userProfileId}`)}
                         className="text-sm italic text-blue-600 hover:underline cursor-pointer mb-1">
-                        by {list.userProfile.userName}
+                        by {data?.userProfile.userName}
                     </p>
 
                     <h3 className="text-gray-700 mb-6 text-lg">
-                        Public: {list.isPublic ? "True" : "False"}
+                        Public: {data?.isPublic ? "True" : "False"}
                     </h3>
                 </div>
 
                 <div className="px-8 mb-6 flex flex-wrap justify-center">
-                    {list.tags?.map((t) => (
+                    {data?.tags?.map((t) => (
                         <p key={t.id} className="text-gray-600 text-sm font-medium inline-block mr-3 mb-2">
                             #{t.name}
                         </p>
@@ -79,13 +60,13 @@ export const ListDetails = ({ loggedInUser }) => {
                 </div>
 
                 <div className="px-8 mb-8">
-                    {list.items?.map((i) => (
+                    {data?.items?.map((i) => (
                         <div key={i.id} className="w-full flex flex-col sm:flex-row items-center justify-between px-4 py-2">
                             <p className="text-gray-600 text-sm font-medium text-center sm:absolute sm:left-1/2 sm:-translate-x-1/2 sm:transform">
                                 {`${i.name} $${i.price}`}
                             </p>
 
-                            {usersList &&
+                            {data?.userProfileId == loggedInUser.id &&
                                 <div className="mt-2 sm:mt-0 sm:ml-auto flex gap-2">
                                     <button onClick={() => { setEditItem(i) }} className="text-blue-600 hover:bg-blue-50 font-medium px-3 py-1 rounded-lg text-sm transition">
                                         Edit
@@ -97,12 +78,14 @@ export const ListDetails = ({ loggedInUser }) => {
                         </div>
                     ))}
                 </div>
-                {usersList && <button onClick={() => { setAddItemModalOpen(true) }} className="text-green-600 hover:bg-green-50 font-medium px-3 py-1 rounded-lg text-lg transition mb-8">Add Item +</button>}
+                {data?.userProfileId == loggedInUser.id && <button onClick={() => { setAddItemModalOpen(true) }} className="text-green-600 hover:bg-green-50 font-medium px-3 py-1 rounded-lg text-lg transition mb-8">Add Item +</button>}
             </div>
 
+            <LoadingModal isLoading={isLoading} />
+            {isError && <ErrorModal error={error} />}
             <EditItemModal editItem={editItem} setEditItem={setEditItem} />
             <AddItemModal isModalOpen={addItemModalOpen} setIsModalOpen={setAddItemModalOpen} newItemArr={newItemArr} setNewItemArr={setNewItemArr} />
-            <DeleteModal deleteByIdFunc={deleteItemById} deleteId={deleteId} setDeleteId={setDeleteId} />
+            <DeleteModal deleteByIdFunc={deleteById} deleteId={deleteId} setDeleteId={setDeleteId} />
         </div >
 
     )
